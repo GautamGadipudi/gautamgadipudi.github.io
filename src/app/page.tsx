f0143a3b3ba58ad38a83ae58617497bb26dcfae0
Gautam Gadipudi <gautam.96.gadipudi@gmail.com>
2025-08-19 15:16:06 -0700

    Update live url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -191,7 +191,7 @@ export default function Home() {
                 title="Minesweeper (CLI, Python)"
                 description="Terminal based minesweeper game implemented in Python. Implemented to brush up on my graph traversal fundamentals."
                 githubUrl="https://github.com/GautamGadipudi/minesweeper-py-cli"
-                liveUrl="https://trinket.io/python3/cf9a677b94be"
+                liveUrl="https://trinket.io/python3/aecd1b63c6d3?outputOnly=true"
                 technologies={["Python", "Graph Traversal", "Game", "CLI"]}
               />
 
